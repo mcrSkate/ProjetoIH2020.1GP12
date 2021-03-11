59c8735714b6a35c2ec8c804fb4086f9a295275d
am3301 <ampass@cin.ufpe.br>
2021-03-11 20:22:51 -0300

    mux_a feito

---
+++ Multiplexadores/mux_a.v
@@ -0,0 +1,20 @@
+module mux_a (
+    input wire  [1:0]  mux_a_control;
+    input wire  [31:0] pc_out;
+    input wire  [31:0] regA_out;
+    input wire  [31:0] mdr_out;
+    output wire [31:0] mux_a_out;
+);
+
+always @(*) begin
+    case(mux_a_control)
+
+        2'b00 : mux_a_out = pc_out;
+        2'b01 : mux_a_out = regA_out;
+        2'b10 : mux_a_out = mdr_out;
+
+    endcase
+end
+
+    
+endmodule

c60969ac453be9400677856a1334162bdf88be79
am3301 <ampass@cin.ufpe.br>
2021-03-11 20:44:56 -0300

    mux iord feito, mux b corrigido

---
+++ Multiplexadores/mux_iord.v
@@ -0,0 +1,21 @@
+module mux_iord(
+    input wire  [1:0]  mux_iord_control;
+    input wire  [31:0] pc_out;
+    input wire  [31:0] aluOut_out;
+    input wire  [31:0] ula_out;
+    input wire  [31:0] exec_out;
+    output wire [31:0] mux_iord_out;
+);
+
+always @(*) begin
+    case (mux_iord_control)
+         
+        2'b00 : mux_iord_out = pc_out;
+        2'b01 : mux_iord_out = aluOut_out;
+        2'b10 : mux_iord_out = ula_out;
+        2'b11 : mux_iord_out = exec_out;
+
+    endcase
+end
+    
+endmodule

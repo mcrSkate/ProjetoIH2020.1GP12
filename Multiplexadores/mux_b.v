f28a39cf13887c0aa4e56cc23d0892ba115c032d
am3301 <ampass@cin.ufpe.br>
2021-03-11 20:34:51 -0300

    mux b feito

---
+++ Multiplexadores/mux_b.v
@@ -0,0 +1,22 @@
+module mux_b (
+    input wire  [1:0]  mux_b_control;
+    input wire  [31:0] regB_out;
+    input wire  [31:0] num4_out;
+    input wire  [31:0] singExtend16_32_out;
+    input wire  [31:0] shiftleft_2_out;
+    output wire [31:0] mux_b_out;
+);
+
+always @(*) begin
+    case(mux_b_control)
+
+        2'b00 : mux_b_out = regB_out;
+        2'b01 : mux_b_out = 32'd4;
+        2'b10 : mux_b_out = singExtend16_32_out;
+        2'b11 : mux_b_control =  shiftleft_2_out;
+
+    endcase
+end
+
+    
+endmodule

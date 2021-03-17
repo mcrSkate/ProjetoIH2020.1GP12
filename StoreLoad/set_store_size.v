508a248ba520aeb8c3617359662137c9f753fd47
Matheus <mcr@cin.ufpe.br>
2021-03-16 21:32:47 -0300

    so falta mult/div

---
+++ StoreLoad/set_store_size.v
@@ -0,0 +1,14 @@
+module set_store_size (
+    input wire [1:0] set_store_size_control;
+    input wire [31:0] b_out;
+    input wire [31:0] memory_data_register_out;
+    output wire [31:0] set_store_size_out;
+);
+    always @(*) begin
+        case (set_store_size_control)
+            assign 1'b00 : set_store_size_out = {memory_data_register_out[31:8],b_out[7:0]};
+            assign 1'b01 : set_store_size_out = {memory_data_register_out[31:16],b_out[15:0]};
+            assign 1'b10 : set_store_size_out = b_out[31:0];
+        endcase
+    end
+endmodule

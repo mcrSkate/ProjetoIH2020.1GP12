739ed1ea83da432196a4662fe4dc5fefcec6142b
Matheus <mcr@cin.ufpe.br>
2021-03-23 22:45:47 -0300

    cpf... cpu cancelada

--- cpu.v
+++
@@ -1,9 +0,0 @@
-module cpu (
-    ports
-);
-
-wire AControl;
-    Registrador A (clock, reset, AControl, br_out_A, regA_out);
-    Registrador B (clock, reset, BControl, br_out_B, regB_out);
-    Banco_reg br (clock, reset, regControl, )
-endmodule
